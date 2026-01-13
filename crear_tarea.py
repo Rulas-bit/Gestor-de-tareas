7a61ec09d8e4a63701d2aabc12c0ee7f8b8cc228
Rulas-bit <rvaquerop03@educarex.es>
2026-01-13 18:56:09 +0100

    Add files via upload

---
+++ crear_tarea.py
@@ -0,0 +1,41 @@
+
+#Crear nueva tarea 
+
+def crear_tarea (tareas):
+        titulo= input("Titulo de la tarea")
+       
+        if titulo == "" :
+                print("el titulo es obligatorio")
+                return
+        # Pedimos descripción opcional.
+
+        descripción= input("descripción (opcional):")
+
+            # Pedimos prioridad y validamos.
+
+        prioridad= input("alta/media/baja :")
+        if prioridad not in ["alta, media, baja "] :
+                prioridad= ("alta") # valor por defecto si se pone algo incorrecto.
+                
+        
+               
+
+ 
+                                                                                                    
+
+
+
+
+
+
+
+
+  
+        
+
+
+        
+      
+        
+
+
